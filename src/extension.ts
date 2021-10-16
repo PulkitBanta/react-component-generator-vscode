f0910cab390789f8b02f264b9cc110a4231f18b2
Pulkit Banta <pulkitbanta01@gmail.com>
2021-10-16 13:33:59 +0530

    feat: create component with specified name and path

--- src/extension.ts
+++ src/extension.ts
@@ -3,23 +3,39 @@ import { addTemplateFiles } from './helper';
 import path = require('path');
 import fs = require('fs');
 
+const componentPath = () =>
+    window.showInputBox({
+        title: 'Specify path for folder',
+        placeHolder: `Example 'src/components'`
+    });
+
+const componentName = () =>
+    window.showInputBox({
+        title: 'Enter name of the component',
+        placeHolder: `Example 'Header'`
+    });
+
+const validateInput = (val: any, errMessage: string): boolean => {
+    if (!val) {
+        window.showErrorMessage(errMessage);
+        return true;
+    }
+    return false;
+};
+
 export function activate(context: ExtensionContext) {
     commands.registerCommand(
         'medly-react-component-generator.create-react-component',
         () => {
-            window
-                .showInputBox({
-                    title: 'Specify path for folder',
-                    placeHolder: 'Example src/components'
-                })
-                .then((directory) => {
-                    if (!directory) {
-                        window.showErrorMessage('No path specified');
-                        return;
-                    }
-                    addTemplateFiles(directory + '/');
-                    window.showInformationMessage(directory + ' provided');
-                });
+            componentPath().then((path) => {
+                if (!validateInput(path, 'No path specified')) {
+                    componentName().then((name) => {
+                        if (!validateInput(name, 'Name is undefined')) {
+                            addTemplateFiles(path + '/', name!);
+                        }
+                    });
+                }
+            });
         }
     );
 }
--- src/helper.ts
+++ src/helper.ts
@@ -2,7 +2,25 @@ import { workspace, window, Uri } from 'vscode';
 import path = require('path');
 import fs = require('fs');
 
-export const templateDir = () => path.join(__dirname + '/../assets/template/');
+const SUFFIX_LENGTH = 9;
+
+export const templateDir = (): string =>
+    path.join(__dirname + '/../assets/template/');
+
+export const filePath = (
+    templateFile: string,
+    componentPath: string,
+    componentName: string
+): string => {
+    const fileName = templateFile.includes('component')
+        ? componentName +
+          templateFile.substring(9, templateFile.length - SUFFIX_LENGTH)
+        : templateFile.substring(0, templateFile.length - SUFFIX_LENGTH);
+
+    return (
+        getWorkspaceFolder() + componentPath + componentName + '/' + fileName
+    );
+};
 
 export const getWorkspaceFolder = (): string | undefined => {
     if (workspace.workspaceFolders !== undefined) {
@@ -16,7 +34,10 @@ export const getWorkspaceFolder = (): string | undefined => {
     }
 };
 
-export const addTemplateFiles = (directory: string) => {
+export const addTemplateFiles = (
+    componentPath: string,
+    componentName: string
+) => {
     fs.readdir(templateDir(), (err, files) => {
         if (err) {
             window.showErrorMessage('files not found');
@@ -30,11 +51,7 @@ export const addTemplateFiles = (directory: string) => {
                         return;
                     }
                     workspace.fs.writeFile(
-                        Uri.file(
-                            getWorkspaceFolder() +
-                                directory +
-                                file.substring(0, file.length - 9)
-                        ),
+                        Uri.file(filePath(file, componentPath, componentName)),
                         content
                     );
                 });
