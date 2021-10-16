84def3c29b85a24623580aa333b694928feecef7
Pulkit Banta <pulkitbanta01@gmail.com>
2021-10-16 12:47:59 +0530

    feat: create component dir and files

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,13 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import { workspace, Uri, commands, window, ExtensionContext } from 'vscode';
-import { getWorkspaceFolder } from './helper';
+import { commands, window, ExtensionContext } from 'vscode';
+import { addTemplateFiles, templateDir } from './helper';
 import path = require('path');
+import fs = require('fs');
 
 export function activate(context: ExtensionContext) {
     commands.registerCommand(
         'medly-react-component-generator.create-react-component',
         () => {
-            workspace.fs
-                .readFile(
-                    Uri.file(
-                        path.join(
-                            __dirname + '/../assets/template/component.template'
-                        )
-                    )
-                )
-                .then((content) => {
-                    workspace.fs.writeFile(
-                        Uri.file(
-                            getWorkspaceFolder() +
-                                '/src/components/Component.tsx'
-                        ),
-                        content
-                    );
-                    window.showInformationMessage(
-                        'Component successfully created'
-                    );
-                });
+            addTemplateFiles();
         }
     );
 }
--- src/helper.ts
+++ src/helper.ts
@@ -1,13 +1,44 @@
-import { workspace, window } from 'vscode';
+import { workspace, window, Uri } from 'vscode';
+import path = require('path');
+import fs = require('fs');
+
+export const templateDir = () => path.join(__dirname + '/../assets/template/');
 
 export const getWorkspaceFolder = (): string | undefined => {
-    if(workspace.workspaceFolders !== undefined) {
+    if (workspace.workspaceFolders !== undefined) {
         let wf = workspace.workspaceFolders[0].uri.path;
-        // let f = workspace.workspaceFolders[0].uri.fsPath ;
         return wf;
-    } 
-    else {    
-        window.showErrorMessage("Try to run this command in an open workspace folder");
+    } else {
+        window.showErrorMessage(
+            'Try to run this command inside a workspace folder'
+        );
         return undefined;
     }
+};
+
+export const addTemplateFiles = () => {
+    fs.readdir(templateDir(), (err, files) => {
+        if (err) {
+            window.showErrorMessage('files not found');
+        } else {
+            files.forEach((file) => {
+                fs.readFile(templateDir() + file, (err, content) => {
+                    if (err) {
+                        window.showErrorMessage(
+                            'Error occurred while reading ' + file
+                        );
+                        return;
+                    }
+                    workspace.fs.writeFile(
+                        Uri.file(
+                            getWorkspaceFolder() +
+                                '/src/components/' +
+                                file.substring(0, file.length - 9)
+                        ),
+                        content
+                    );
+                });
+            });
+        }
+    });
 };
